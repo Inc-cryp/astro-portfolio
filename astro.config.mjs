f1dafcedc42dcf9ac0450a7f6cf2ed1d319d7952
Abdillah Fazri <abdillahfazri003@gmail.com>
2026-02-02 03:00:48 +0700

    hf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'node:url';
-
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   site: 'https://yourdomain.com', // Replace with your actual domain
-  integrations: [react(), sitemap()],
+  integrations: [react(), tailwind(), sitemap()],
   prefetch: true,
 
   vite: {
-    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
